d46d0f440e16ca639a75a1e29bb167b58aa6498d
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2019-05-26 23:00:50 +0200

    WIP: First allocator using normal malloc.

---
+++ ompss_allocator.hpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2019  Jimmy Aguilar Mena
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OMPSS_ALLOCATOR_HPP
+#define OMPSS_ALLOCATOR_HPP
+
+#include <cstddef>
+#include <cstring>
+#include <cassert>
+#include <cstdlib>
+
+template<typename _Tp>
+class ompss_allocator {
+public:
+	typedef size_t     size_type;
+	typedef ptrdiff_t  difference_type;
+	typedef _Tp*       pointer;
+	typedef const _Tp* const_pointer;
+	typedef _Tp&       reference;
+	typedef const _Tp& const_reference;
+	typedef _Tp        value_type;
+
+	ompss_allocator() = default;
+
+	_Tp *allocate(std::size_t n)
+	{
+		if (n > std::size_t(-1) / sizeof(_Tp))
+			throw std::bad_alloc();
+
+		_Tp * const p = static_cast<_Tp*>(std::malloc(n * sizeof(_Tp)));
+		if (!p)
+			throw std::bad_alloc();
+
+		return p;
+	 }
+
+	void deallocate(_Tp* p, std::size_t n) noexcept
+	{
+		(void *) n;
+		std::free(p);
+	}
+};
+
+#endif // OMPSS_ALLOCATOR_HPP
